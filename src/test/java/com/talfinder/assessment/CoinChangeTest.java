65c51cf8e0974b0ccbf98a5cc4a3483592872f9f
hbhargav <harsha.bhargav@hebbalelabs.com>
2019-03-13 12:39:31 +0530

    Updated Test Case to be Parametrized

--- src/test/java/com/talfinder/assessment/CoinChangeTest.java
+++ src/test/java/com/talfinder/assessment/CoinChangeTest.java
@@ -1,52 +1,50 @@
 package com.talfinder.assessment;
 
+import java.util.Arrays;
+import java.util.Collection;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  * Created by hbhargav on 08/03/19.
  */
+
+@RunWith(Parameterized.class)
 public class CoinChangeTest {
 
-  @Test
-  public void checkPositiveCase() {
-    int[] coins = new int[]{1, 2, 5};
-    int numCoins = CoinChange.minCoins(coins, 11);
-    Assert.assertEquals(3, numCoins);
+  @Parameters
+  public static Collection<Object[]> parameters() {
+    return Arrays.asList(new Object[][] {
+        {new int[]{1, 2, 5}, 11, 3},
+        {new int[]{1, 5, 10}, 8, 4},
+        {new int[]{1, 5, 10}, 10, 1},
+        {new int[]{2}, 3, -1},
+        {new int[]{5, 10}, 24, -1},
+        {new int[]{5, 10}, 64, -1},
+        {new int[]{}, 3, -1},
+        {new int[]{}, -12, -1},
+        {new int[]{}, -44, -1}
+    });
+  }
 
-    coins = new int[]{1, 5, 10};
-    numCoins = CoinChange.minCoins(coins, 8);
-    Assert.assertEquals(4, numCoins);
+  private final int[] coins;
+  private final int amount;
+  private final int expectedNumCoins;
 
-    numCoins = CoinChange.minCoins(coins, 10);
-    Assert.assertEquals(1, numCoins);
+  public CoinChangeTest(int[] coins, int amount, int expectedNumCoins) {
+    this.coins = coins;
+    this.amount = amount;
+    this.expectedNumCoins = expectedNumCoins;
   }
 
   @Test
-  public void checkNegativeCase() {
-    int[] coins = new int[]{2};
-    int numCoins = CoinChange.minCoins(coins, 3);
-    Assert.assertEquals(-1, numCoins);
-
-    coins = new int[]{5, 10};
-    numCoins = CoinChange.minCoins(coins, 24);
-    Assert.assertEquals(-1, numCoins);
-
-    numCoins = CoinChange.minCoins(coins, 64);
-    Assert.assertEquals(-1, numCoins);
-  }
-
-  @Test
-  public void checkInvalidCase() {
-    int[] coins = new int[]{};
-    int numCoins = CoinChange.minCoins(coins, 3);
-    Assert.assertEquals(-1, numCoins);
-
-    coins = new int[]{};
-    numCoins = CoinChange.minCoins(coins, -12);
-    Assert.assertEquals(-1, numCoins);
-
-    numCoins = CoinChange.minCoins(coins, -44);
-    Assert.assertEquals(-1, numCoins);
+  public void checkCoinChange() {
+    Assert.assertEquals("input:Coins as " + Arrays.toString(coins) + " and Amount as "+ amount , expectedNumCoins, CoinChange.minCoins(coins, amount));
   }
 }
